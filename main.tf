e4f956f4993a9da01450d043d49adc40134b18f2
Sean Sundberg <seansund@us.ibm.com>
2020-06-29 23:47:32 -0500

    Adds implementation for key protect provisioning

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "ibm" {
+  version = ">= 1.2.1"
+}
+
+data "ibm_resource_group" "tools_resource_group" {
+  name = var.resource_group_name
+}
+
+locals {
+  name_prefix     = var.name_prefix != "" ? var.name_prefix : var.resource_group_name
+}
+
+resource "ibm_resource_instance" "studio_instance" {
+  name              = "${replace(local.name_prefix, "/[^a-zA-Z0-9_\\-\\.]/", "")}-keyprotect"
+  service           = "kms"
+  plan              = var.plan
+  location          = var.resource_location
+  resource_group_id = data.ibm_resource_group.tools_resource_group.id
+  tags              = var.tags
+
+  timeouts {
+    create = "15m"
+    update = "15m"
+    delete = "15m"
+  }
+}
